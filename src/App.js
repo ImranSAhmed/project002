7998a8c610d5d1fb3d1bdf896d962d3116eea4dd
ImranSAhmed <74242936+ImranSAhmed@users.noreply.github.com>
2020-11-14 13:35:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>Updated by Imran</br>       
+          <br/>
+          Updated by Imran     
         </p>
         <a
           className="App-link"
