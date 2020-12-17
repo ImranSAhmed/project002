de0db2df5db2eeb6a60884e5fbd785fae689c6d3
ImranSAhmed <74242936+ImranSAhmed@users.noreply.github.com>
2020-12-17 17:33:06 +0500

    Hooks

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./room.css";
 
+function Room() {
+  const [isLit, SetLit] = React.useState(true);
+
+  function Test() {
+    SetLit(!isLit);
+  }
+
+  return (
+    <div className={`room ${isLit? "lit" : "dark"}`}>
+    <div>
+    Room is {isLit ? "Lit" : "Dark"}
+     <br/>
+      <button onClick={Test}>Click Me</button>
+     
+      <p>Current value of Lit = {isLit.toString()}</p>
+    </div>
+     
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="IMRAN Ahmed"/>
+    <Room />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+/* ReactDOM.render(
+  <React.StrictMode>
+    <App name="IMRAN Ahmed" />
+  </React.StrictMode>,
+  document.getElementById("root")
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
